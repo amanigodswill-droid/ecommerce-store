1423971c6a7e6d554e620b95046823f09205cdd2
amanigodswill-droid <amanigodswill@gmail.com>
2026-07-27 15:20:24 +0300

    Added controlled search functionality for products

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 import ProductCard from "../components/ProductCard";
+import SearchBar from "../components/SearchBar";
 
 function Products() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [search, setSearch] = useState("");
 
+  
   useEffect(() => {
     axios
       .get("https://fakestoreapi.com/products")
@@ -22,26 +25,44 @@ function Products() {
       });
   }, []);
 
+  
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+
   if (loading) {
     return <h2 className="message">Loading products...</h2>;
   }
 
+  
   if (error) {
     return <h2 className="message">{error}</h2>;
   }
 
   return (
     <div className="container">
-      <h1 className="page-title">Products</h1>
+
+      <h1 className="page-title">
+        Products
+      </h1>
+
+      <SearchBar
+        search={search}
+        setSearch={setSearch}
+      />
 
       <div className="product-grid">
-        {products.map((product) => (
+
+        {filteredProducts.map((product) => (
           <ProductCard
             key={product.id}
             product={product}
           />
         ))}
+
       </div>
+
     </div>
   );
 }
